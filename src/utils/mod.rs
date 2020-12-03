e308c95a97f8b7d540303498db3bea1c02158b4d
maslabgamer <maslabgamer@gmail.com>
2020-12-03 10:38:16 -0800

    Initial commit

---
+++ src/utils/mod.rs
@@ -0,0 +1,11 @@
+use std::path::Path;
+use std::fs::File;
+use std::io::{BufReader, BufRead};
+
+pub(crate) fn read_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("No such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|line| line.expect("Could not parse line"))
+        .collect()
+}
