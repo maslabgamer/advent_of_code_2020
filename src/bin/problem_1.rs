02ba9f41c680a6928737b8c4711445f11c132aa1
maslabgamer <maslabgamer@gmail.com>
2020-12-03 13:46:29 -0800

    Adds doc code to utils
    Explicitly types numbers var in problem 1 so not implicitly done in collection

--- src/bin/problem_1.rs
+++ src/bin/problem_1.rs
@@ -5,10 +5,10 @@ fn main() {
 }
 
 fn process_input() -> i32 {
-    let numbers = read_file("resources/problem_1_input.txt")
+    let numbers: Vec<i32> = read_file("resources/problem_1_input.txt")
         .iter()
         .map(|number| number.parse::<i32>().unwrap())
-        .collect::<Vec<i32>>();
+        .collect();
     for i in 0..numbers.len() - 2 {
         for j in i + 1..numbers.len() - 1 {
             for k in j + 1..numbers.len() {
--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -2,6 +2,8 @@ use std::path::Path;
 use std::fs::File;
 use std::io::{BufReader, BufRead};
 
+/// Returns the contents of a file as a Vector of Strings where each String is a line in the file
+/// These are expected to be files from the resources folder, so no error handling is being done
 pub fn read_file(filename: impl AsRef<Path>) -> Vec<String> {
     let file = File::open(filename).expect("No such file");
     let buf = BufReader::new(file);
