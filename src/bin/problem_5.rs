12a27c7a329f4aed6e91afe84fe59586fee24abb
maslabgamer <maslabgamer@gmail.com>
2020-12-05 14:37:50 -0800

    Removes a space that bothered me
    Cause I can

--- src/bin/problem_5.rs
+++ src/bin/problem_5.rs
@@ -22,7 +22,7 @@ fn decode_all_passes() -> Vec<i32> {
 
 fn decode_pass(boarding_pass: &str) -> i32 {
     let pass: String = boarding_pass.chars()
-        .map(|c| (['B', 'R'].contains(&c) as i32).to_string() )
+        .map(|c| (['B', 'R'].contains(&c) as i32).to_string())
         .collect();
     i32::from_str_radix(&pass, 2).unwrap()
 }
