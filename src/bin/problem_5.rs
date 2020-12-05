1813944aff513694bb8cfff3c376e6044751297b
maslabgamer <maslabgamer@gmail.com>
2020-12-05 14:37:14 -0800

    Still binary, but slightly more cursed

--- src/bin/problem_5.rs
+++ src/bin/problem_5.rs
@@ -8,8 +8,7 @@ fn main() {
 
 fn find_missing_seat(all_passes: &mut Vec<i32>) -> i32 {
     all_passes.sort();
-    (all_passes[0]..*all_passes.last().unwrap())
-        .into_iter()
+    (all_passes[0]..*all_passes.last().unwrap()).into_iter()
         .filter(|&seat| !all_passes.contains(&(seat as i32)))
         .last().unwrap()
 }
@@ -23,11 +22,7 @@ fn decode_all_passes() -> Vec<i32> {
 
 fn decode_pass(boarding_pass: &str) -> i32 {
     let pass: String = boarding_pass.chars()
-        .map(|c| match c {
-            'F' | 'L' => '0',
-            'B' | 'R' => '1',
-            _ => panic!("Invalid character!")
-        })
+        .map(|c| (['B', 'R'].contains(&c) as i32).to_string() )
         .collect();
     i32::from_str_radix(&pass, 2).unwrap()
 }
