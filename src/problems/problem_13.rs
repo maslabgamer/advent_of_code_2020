7a2768cc4c82b3efd7b003827fea473a4ac5842b
maslabgamer <maslabgamer@gmail.com>
2020-12-13 12:44:16 -0800

    Slight part 1 optimization
    Computes "minutes_to_wait" at end instead of on each loop

--- src/problems/problem_13.rs
+++ src/problems/problem_13.rs
@@ -2,11 +2,13 @@ extern crate ring_algorithm;
 
 use ring_algorithm::chinese_remainder_theorem;
 
+const U32_MAX: u32 = u32::MAX;
+
 pub fn find_earliest_bus(input: &[u8]) -> u32 {
     let (earliest_departure, read_count) = lexical::parse_partial::<u32, _>(&input[..]).unwrap();
     let mut input = &input[read_count + 1..]; // Make sure we skip the line break
 
-    let mut earliest_bus = (earliest_departure, 0);
+    let mut earliest_bus = (U32_MAX, 0);
     while let Some(first_char) = input.first() {
         if *first_char == b'x' {
             input = &input[2..]; // Make sure we also skip the comma
@@ -14,14 +16,13 @@ pub fn find_earliest_bus(input: &[u8]) -> u32 {
             let (bus_id, read_count) = lexical::parse_partial::<u32, _>(&input[..]).unwrap();
             // Now find how many times we have to multiply the "time" by to get to my departure time
             let departure_time = (bus_id * (earliest_departure / bus_id)) + bus_id;
-            let minutes_to_wait = departure_time - earliest_departure;
-            if minutes_to_wait < earliest_bus.0 {
-                earliest_bus = (minutes_to_wait, bus_id);
+            if departure_time < earliest_bus.0 {
+                earliest_bus = (departure_time, bus_id);
             }
             input = &input[read_count + 1..]; // Next character guaranteed to be comma to skip
         }
     }
-    earliest_bus.0 * earliest_bus.1
+    earliest_bus.1 * (earliest_bus.0 - earliest_departure)
 }
 
 pub fn find_sequential_arrivals(input: &[u8]) -> i128 {
