592b79335fdf9ddef3433e8256a3127464329f0c
maslabgamer <maslabgamer@gmail.com>
2020-12-14 16:17:13 -0800

    Updates benchmark names for clarity
    Prevents overlap in results determination

--- benches/problem_12_benchmark.rs
+++ benches/problem_12_benchmark.rs
@@ -3,7 +3,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use aoc_2020::ship::run_base;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("problem_12", |b| b.iter(|| run_base()));
+    c.bench_function("Problem 12 Both Parts", |b| b.iter(|| run_base()));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/problem_13_benchmark.rs
+++ benches/problem_13_benchmark.rs
@@ -4,8 +4,8 @@ use aoc_2020::problems::problem_13::{find_earliest_bus, find_sequential_arrivals
 
 pub fn part_13_benchmark(c: &mut Criterion) {
     let input = include_bytes!("../resources/problem_13_input.txt");
-    c.bench_function("Part 1", |b| b.iter(|| find_earliest_bus(input)));
-    c.bench_function("Part 2", |b| b.iter(|| find_sequential_arrivals(input)));
+    c.bench_function("Problem 13 Part 1", |b| b.iter(|| find_earliest_bus(input)));
+    c.bench_function("Problem 13 Part 2", |b| b.iter(|| find_sequential_arrivals(input)));
 }
 
 criterion_group!(benches, part_13_benchmark);
